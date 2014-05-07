7dc08c0c68bb86eec6d9bc04e6cb16ddbcc8c5b0
Mark Turner <mturner@webassign.net>
2014-05-07 11:41:52 -0400

    removed provider references

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,6 @@ class nrpe (
   $config        = $nrpe::params::nrpe_config,
   $include_dir   = $nrpe::params::nrpe_include_dir,
   $package_name  = $nrpe::params::nrpe_packages,
-  $provider      = $nrpe::params::nrpe_provider,
   $purge         = undef,
   $recurse       = undef,
   $service_name  = $nrpe::params::nrpe_service,
@@ -32,7 +31,6 @@ class nrpe (
 
   package { $package_name:
     ensure   => installed,
-    #   provider => $provider,
   }
 
   service { 'nrpe_service':
