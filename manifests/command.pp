80cc6042bb891dd21638ae40bdd63373baaf7e6b
chadq <chad.quimby@paperg.com>
2014-11-07 15:07:46 -0800

    revert

--- manifests/command.pp
+++ manifests/command.pp
@@ -1,7 +1,6 @@
 #
 define nrpe::command (
   $command,
-  $sudo         = false,
   $ensure       = present,
   $include_dir  = $nrpe::params::nrpe_include_dir,
   $libdir       = $nrpe::params::libdir,
@@ -11,7 +10,6 @@ define nrpe::command (
 ) {
 
   file { "${include_dir}/${title}.cfg":
-    sudo    => $sudo,
     ensure  => $ensure,
     content => template('nrpe/command.cfg.erb'),
     owner   => root,
