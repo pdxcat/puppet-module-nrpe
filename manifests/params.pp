98a8d5fe336ece79585c8de72dad579b5ba61cac
Guy Matz <gmatz@renttherunway.com>
2015-04-13 12:09:42 -0400

    removed silly commas

--- manifests/params.pp
+++ manifests/params.pp
@@ -122,8 +122,8 @@ class nrpe::params {
       ]
     }
     default:   {
-      $dont_blame_nrpe = $nrpe::params::dont_blame_nrpe,
-      $command_prefix = undef,
+      $dont_blame_nrpe = 0
+      $command_prefix  = undef
     }
   }
 
