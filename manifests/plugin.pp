5fa0da9c699e33dadc8e8bae41cb5148f06deb61
Christoph Tavan <christoph@mbr-targeting.com>
2015-10-08 17:24:34 +0000

    Require either $content or $source to be set in nrpe::plugin.
    If none of the two are set puppet will still create an empty executable
    file which will result in the nrpe error message:
    "NRPE: Unable to read output"
    
    However the check will still appear as OK in nagios. This patch
    explicitly checks that precisely one of the two options is set.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -8,6 +8,12 @@ define nrpe::plugin (
   $package_name = $nrpe::params::nrpe_packages,
   $file_group   = $nrpe::params::nrpe_files_group,
 ) {
+  if ($content == undef and $source == undef) {
+    fail("nrpe::plugin[$title]: Either \$content or \$source must be defined!")
+  }
+  if ($content != undef and $source != undef) {
+    fail("nrpe::plugin[$title]: \$content and \$source must not both be defined!")
+  }
   file { "${libdir}/${title}":
     ensure  => $ensure,
     content => $content,
--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -9,11 +9,29 @@ describe 'nrpe::plugin', :type => :define do
     end
 
     let (:title) {'check_users'}
-    let :params do
-      {
-        :ensure  => 'present',
-      }
+
+    describe 'should check for $content and $source' do
+      let :params do
+        {
+          :ensure  => 'present',
+        }
+      end
+
+      it do
+        expect {
+          should contain_file('/usr/lib/nagios/plugins/check_users')
+        }.to raise_error(Puppet::Error, /Either \$content or \$source must be defined!/)
+      end
     end
 
-    it { should contain_file('/usr/lib/nagios/plugins/check_users') }
+    describe 'should contain plugin file' do
+      let :params do
+        {
+          :ensure  => 'present',
+          :content => 'exit 0',
+        }
+      end
+
+      it { should contain_file('/usr/lib/nagios/plugins/check_users') }
+    end
 end
