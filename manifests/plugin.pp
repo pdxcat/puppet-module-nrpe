8e8fd61f0012b12410ded789799aa626f8c7242f
Jan-Otto Kröpke <jkroepke@users.noreply.github.com>
2015-12-04 11:21:09 +0100

    Added selinux parameter in nrpe::plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -7,14 +7,23 @@ define nrpe::plugin (
   $libdir       = $nrpe::params::libdir,
   $package_name = $nrpe::params::nrpe_packages,
   $file_group   = $nrpe::params::nrpe_files_group,
+  $selrange     = undef,
+  $selrole      = undef,
+  $seltype      = undef,
+  $seluser      = undef,
+ 
 ) {
   file { "${libdir}/${title}":
-    ensure  => $ensure,
-    content => $content,
-    source  => $source,
-    owner   => 'root',
-    group   => $file_group,
-    mode    => $mode,
-    require => Package[$package_name],
+    ensure   => $ensure,
+    content  => $content,
+    source   => $source,
+    owner    => 'root',
+    group    => $file_group,
+    mode     => $mode,
+    require  => Package[$package_name],
+    selrange => $selrange,
+    selrole  => $selrole,
+    seltype  => $seltype,
+    seluser  => $seluser,
   }
 }
